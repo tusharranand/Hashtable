814f44efe1107fcd4f559bee7610fb1881336635
Tushar Anand <tusharranand@gmail.com>
2022-03-29 08:55:12 +0530

    [Tushar Anand] Added: Ability to check fequency of words in a phrase

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -7,6 +7,20 @@ namespace HashTable
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hashtable Program");
+            Linked_Hash_Map<string, int> linked_hash_map = new Linked_Hash_Map<string, int>(5);
+
+            string Sentence = "to be or not to be";
+            string[] Words = Sentence.Split(" ");
+            foreach (string word in Words)
+            {
+                int Value = linked_hash_map.Get(word);
+                if (Value == default)
+                    Value = 1;
+                else Value++;
+                linked_hash_map.Add(word, Value);
+            }
+            int Frequency = linked_hash_map.Get("to");
+            Console.WriteLine(Frequency);
         }
     }
 }
