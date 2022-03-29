2a35f25659c9945abf49bdbc3722078d8dcdbefe
Tushar Anand <tusharranand@gmail.com>
2022-03-29 12:31:48 +0530

    [Tushar Anand] Added: Ability to check fequency of words in a paragraph

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -9,7 +9,8 @@ namespace HashTable
             Console.WriteLine("Welcome to Hashtable Program");
             Linked_Hash_Map<string, int> linked_hash_map = new Linked_Hash_Map<string, int>(5);
 
-            string Sentence = "to be or not to be";
+            string Sentence = "paranoids are not paranoid because they are paranoid but because " +
+                "they keep putting themselves deliberately into paranoid avoidable situations";
             string[] Words = Sentence.Split(" ");
             foreach (string word in Words)
             {
@@ -19,8 +20,12 @@ namespace HashTable
                 else Value++;
                 linked_hash_map.Add(word, Value);
             }
-            int Frequency = linked_hash_map.Get("to");
-            Console.WriteLine(Frequency);
+            int Frequency = linked_hash_map.Get("paranoid");
+            Console.WriteLine("\"paranoid\" comes {0} times in the given paragraph", Frequency);
+            Frequency = linked_hash_map.Get("are");
+            Console.WriteLine("\"are\" comes {0} times in the given paragraph", Frequency);
+            Frequency = linked_hash_map.Get("they");
+            Console.WriteLine("\"they\" comes {0} times in the given paragraph", Frequency);
         }
     }
 }
